f5486f1d4756188bc29dba75a8bc65b43aec8eb2
Daniel Flores <d.flores042@gmail.com>
2018-07-01 16:58:47 -0400

    initialize repo

---
+++ lib/flores_view_tool.rb
@@ -0,0 +1,6 @@
+require "flores_view_tool/version"
+require "flores_view_tool/renderer"
+
+module FloresViewTool
+  # Your code goes here...
+end
---
+++ lib/flores_view_tool/version.rb
@@ -0,0 +1,3 @@
+module FloresViewTool
+  VERSION = "0.1.0"
+end
